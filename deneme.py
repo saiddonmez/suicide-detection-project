d153e2e7bdf4644be5375684ca046373bf5d36b4
goktugsyvz <goktugsamiyavuz@gmail.com>
2022-04-05 17:15:43 +0300

    initial commit from goktugsyvz

--- deneme.py
+++ deneme.py
@@ -1,1 +1,40 @@
 print('mal baran')
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import re
+import string
+
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.calibration import CalibratedClassifierCV
+from imblearn.under_sampling import InstanceHardnessThreshold
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
+from sklearn.svm import LinearSVC
+
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+from sklearn.feature_selection import SelectFromModel
+
+from imblearn.pipeline import Pipeline
+import pickle
+
+
+import spacy
+
+nlp = spacy.load("en")
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
